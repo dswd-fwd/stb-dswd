22b94b410c3edfd09a0937ac498b568e528d9e7e
REiman CAncino <jrbcancino27@gmail.com>
2025-05-27 09:26:21 +0800

    Create ContactUs.vue

---
+++ resources/js/pages/public/ContactUs.vue
@@ -0,0 +1,14 @@
+<template>
+  <FrontendAppLayout>
+    <main class="p-6">
+      <h1 class="text-3xl font-bold mb-4">Contact Us</h1>
+      <p class="text-lg text-gray-700 mb-8">
+        If you have any questions, feedback, or inquiries, please feel free to reach out to us.
+        We'd love to hear from you!
+      </p>
+    </main>
+  </FrontendAppLayout>
+</template>
+
+<script setup>
+</script>
