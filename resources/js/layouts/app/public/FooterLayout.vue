aeaae78e31d725899dbc71645aa3679ac1635532
REiman CAncino <jrbcancino27@gmail.com>
2025-05-27 09:26:18 +0800

    Update FooterLayout.vue

--- resources/js/layouts/app/public/FooterLayout.vue
+++ resources/js/layouts/app/public/FooterLayout.vue
@@ -9,30 +9,31 @@
                 src="/img/govph-seal-mono-footer.jpg" 
             />
             <div>
-                <h5 class="font-medium">Republic of the Philippines</h5>
-                <p class="max-w-md mt-1">All content is in the public domain unless otherwise stated.</p>
+                <h5 class="font-small">Republic of the Philippines</h5>
+                <p class="max-w-md mt-1 text-xs">All content is in the public domain unless otherwise stated.</p>
             </div>
             <div>
-                <h5 class="font-medium">About GOVPH</h5>
-                <p class="max-w-md mt-1">Learn more about the Philippine government, its structure, how government works and the people behind it.</p>
+                <h5 class="font-small">About GOVPH</h5>
+                <p class="max-w-md mt-1 text-xs">Learn more about the Philippine government, its structure, how government works and the people behind it.</p>
 
                 <div class="flex flex-col mt-4">
-                    <a>GOV.PH</a>
-                    <a>Open Data Portal</a>
-                    <a>Official Gazette</a>
+                    <a class="text-xs" href="https://www.gov.ph" target="_blank" rel="noopener">GOV.PH</a>
+                    <a class="text-xs" href="https://www.gov.ph/data">Open Data Portal</a>
+                    <a class="text-xs" href="https://officialgazette.gov.ph">Official Gazette</a>
                 </div>
+
             </div>
             <div>
-                <h5 class="font-medium">Government Links</h5>
+                <h5 class="font-small">Government Links</h5>
 
-                <div class="flex flex-col mt-1">
-                    <a>Office of the President</a>
-                    <a>Office of the Vice President</a>
-                    <a>Senate of the Philippines</a>
-                    <a>House of Representatives</a>
-                    <a>Supreme Court</a>
-                    <a>Court of Appeals</a>
-                    <a>Sandiganbayan</a>
+                <div class="flex flex-col mt-1 text-xs">
+                    <a href="https://op-proper.gov.ph/">Office of the President</a>
+                    <a href="https://ovp.gov.ph">Office of the Vice President</a>
+                    <a href="https://web.senate.gov.ph">Senate of the Philippines</a>
+                    <a href="https://congress.gov.ph">House of Representatives</a>
+                    <a href="https://sc.judiciary.gov.ph">Supreme Court</a>
+                    <a href="https://ca.judiciary.gov.ph">Court of Appeals</a>
+                    <a href="https://sb.judiciary.gov.ph">Sandiganbayan</a>
                 </div>
             </div>
         </div>
