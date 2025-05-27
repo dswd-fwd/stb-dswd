27a1fc1bc8be69f0794ac63289a36b035a1451e9
REiman CAncino <jrbcancino27@gmail.com>
2025-05-27 09:26:50 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/contact-us', function () {
+    return Inertia::render('public/ContactUs');
+})->name('contact us');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
